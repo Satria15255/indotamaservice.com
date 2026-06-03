fef52d9d16ddee1bc2441e9f9b0b02d5454e1054
satriazafarani <satriazafarani57@gmail.com>
2026-06-03 09:52:12 +0700

    fisrt commit : hero section

--- src/app/page/about.js
+++ src/app/page/about.js
@@ -12,22 +12,21 @@ const About = () => {
       transition={{ duration: 1.0 }} // Animasi selama 1 detik
       viewport={{ once: true }}
     >
-      <div className=" grid grid-cols-1 items-center p-4 ">
+      <div className=" grid grid-cols-1 items-center p-4 py-9">
         <h1 className="text-4xl text-center font-bold ">
-          TENTANG <span className="text-blue-400">KAMI</span>
+          Who's <span className="text-blue-400">We? </span>
         </h1>
 
         <div className="flex grid grid-cols-1 md:grid-cols-2 justify-center mt-2 ">
           <Image src="/assets/about.jpg" alt="About Image" width="800" height="800" className="w-full  mt-16" priority />
           <div className="flex flex-col justify-center">
-            <p className="text-xl font-bold py-3">Servis AC Terpercaya</p>
+            <p className="text-xl font-bold py-3">Trusted AC Services</p>
             <p className="max-w-xl text-sm xl:text-lg pb-3">
-              Kami adalah penyedia layanan AC terpercaya yang berkomitmen untuk memberikan layanan berkualitas tinggi kepada pelanggan kami. Dengan tim teknisi yang terlatih dan berpengalaman, kami siap memberikan solusi terbaik untuk semua
-              kebutuhan perawatan dan perbaikan AC Anda. Kami mengutamakan keselamatan, kenyamanan, dan kepuasan pelanggan dalam setiap langkah layanan kami. Percayakan perawatan AC Anda kepada kami untuk hasil yang optimal dan kinerja AC
-              yang sangat baik setiap saat.
+              We are a trusted AC service provider committed to providing high-quality service to our customers. With a team of experienced and knowledgeable technicians, we are ready to provide the best solutions for all your AC
+              maintenance and repair needs. We prioritize customer safety, comfort, and satisfaction in every step of our service. Trust your AC maintenance to us for optimal results and excellent performance at all times.
             </p>
             <div className="bg-blue-500 pb-3 w-2/5 mt-3">
-              <button className="bg-blue-400 text-white py-4 w-full px-4 ">Tanyakan</button>
+              <button className="bg-blue-400 text-white py-4 w-full px-4 ">Consultation</button>
             </div>
           </div>
         </div>
--- src/app/page/home.js
+++ src/app/page/home.js
@@ -12,15 +12,20 @@ const Home = () => {
       transition={{ duration: 1.0 }} // Animasi selama 1 detik
       viewport={{ once: true }}
     >
-      <div className="flex h-screen grid grid-cols-1 md:grid-cols-2 mt-17 items-center  p-4 justify-center">
-        <div className="flex flex-col order-2 md:order-1">
-          <h1 className="text-4xl xl:text-6xl font-bold text-blue-400 py-4">Servis AC Terpercaya</h1>
-          <p className="max-w-xl text-gray-500 text:xs md:text-lg xl:text-xl">Kami menyediakan perawatan, perbaikan dan pemasangan AC dengan teknisi berpengalaman.</p>
-          <div className="bg-blue-500 pb-3 w-2/5 mt-3">
-            <button className="bg-blue-400 text-white py-4 w-full px-4 ">Whatsapp</button>
+      <div className="flex h-screen grid grid-cols-1 md:grid-cols-2 mt-17 items-center bg-white justify-center">
+        <div className="w-auto p-7 flex flex-col">
+          <p className="text-sm bg-blue-100 rounded-full w-50 flex justify-center items-center p-1">Stay Cool All Year Round</p>
+          <h1 className="text-4xl lg:text-6xl font-bold  py-4">
+            Fast, Reliable & Affordable Air Conditioning<span className="text-blue-800"> Services</span>
+          </h1>
+          <p className="max-w-xl text-gray-500 text:xs md:text-lg ">Professional air conditioning solutions for homes and businesses. Available for installation, cleaning, maintenance, and emergency repairs.</p>
+          <div className=" w-2/5 mt-3">
+            <button className="bg-blue-900 rounded-4xl hover:bg-white hover:text-black transition duration-200 border border-gray-300 text-white py-4 w-full px-4 ">Book Now</button>
           </div>
         </div>
-        <Image src="/assets/home2.jpg" alt="Home Image" width="800" height="800" priority className=" order-1 md:order-2" />
+        <div className="w-full h-screen py-4">
+          <img src="/assets/imageHero.png" alt="Home Image" className="w-full h-full object-cover object-center rounded-l-4xl " />
+        </div>
       </div>
     </motion.div>
   );
