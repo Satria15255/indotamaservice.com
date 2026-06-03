9bbd43399bc0ba98da862c9ee86ee945cc887332
satriazafarani <satriazafarani57@gmail.com>
2026-06-03 10:58:17 +0700

    update hero section : added label

--- src/app/page/home.js
+++ src/app/page/home.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import Image from "next/image";
+import { PiMedal, PiShieldCheckBold, PiClock } from "react-icons/pi";
 
 const Home = () => {
   return (
@@ -16,11 +16,31 @@ const Home = () => {
         <div className="w-auto p-7 flex flex-col">
           <p className="text-sm bg-blue-100 rounded-full w-50 flex justify-center items-center p-1">Stay Cool All Year Round</p>
           <h1 className="text-4xl lg:text-6xl font-bold  py-4">
-            Fast, Reliable & Affordable Air Conditioning<span className="text-blue-800"> Services</span>
+            Fast, Reliable & Affordable Air Conditioning<span className="text-blue-900"> Services</span>
           </h1>
           <p className="max-w-xl text-gray-500 text:xs md:text-lg ">Professional air conditioning solutions for homes and businesses. Available for installation, cleaning, maintenance, and emergency repairs.</p>
           <div className=" w-2/5 mt-3">
             <button className="bg-blue-900 rounded-4xl hover:bg-white hover:text-black transition duration-200 border border-gray-300 text-white py-4 w-full px-4 ">Book Now</button>
+          </div>
+          <div className="flex gap-7 mt-12">
+            <div className="flex items-center gap-2">
+              <PiClock size={40} />
+              <p>
+                Fast <br /> Response
+              </p>
+            </div>
+            <div className="flex items-center gap-2">
+              <PiMedal size={40} />
+              <p>
+                Expert <br /> Technicians
+              </p>
+            </div>
+            <div className="flex items-center gap-2">
+              <PiShieldCheckBold size={40} />
+              <p>
+                Guaranteed <br /> Satisfaction
+              </p>
+            </div>
           </div>
         </div>
         <div className="w-full h-screen py-4">
